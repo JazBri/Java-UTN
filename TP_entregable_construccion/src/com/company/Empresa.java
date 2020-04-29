8716b4a5a35a5b839189d4a665f33b1cea4a8aa7
JazBri <48839855+JazBri@users.noreply.github.com>
2020-04-29 08:41:06 -0300

    Update Empresa.java

--- TP_entregable_construccion/src/com/company/Empresa.java
+++ TP_entregable_construccion/src/com/company/Empresa.java
@@ -7,4 +7,46 @@ public class Empresa {
     private String name;
     private ArrayList<Empleado> totalEmployees;
     private ArrayList<Obra> activeWorks;
+
+    public Empresa(String name, ArrayList<Empleado> totalEmployees, ArrayList<Obra> activeWorks) {
+        this.name = name;
+        this.totalEmployees = totalEmployees;
+        this.activeWorks = activeWorks;
+    }
+
+    public Empresa() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Empleado> getTotalEmployees() {
+        return totalEmployees;
+    }
+
+    public void setTotalEmployees(ArrayList<Empleado> totalEmployees) {
+        this.totalEmployees = totalEmployees;
+    }
+
+    public ArrayList<Obra> getActiveWorks() {
+        return activeWorks;
+    }
+
+    public void setActiveWorks(ArrayList<Obra> activeWorks) {
+        this.activeWorks = activeWorks;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "name='" + name + '\'' +
+                ", totalEmployees=" + totalEmployees +
+                ", activeWorks=" + activeWorks +
+                '}';
+    }
 }
